dc73c39d25ad028821c88014fad3b7d331a7386d
Gab Dev <suppershock777@gmail.com>
2024-04-16 13:51:44 -0300

    Feat: Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./**/*.{html,js}"],
+   theme: {
+      extend: {
+         colors: {
+            primary: "#F5F5F5",
+            amarelo: "#FFBF00",
+            branco: "#FFFFFF",
+         },
+      },
+   },
+   plugins: [],
+};
